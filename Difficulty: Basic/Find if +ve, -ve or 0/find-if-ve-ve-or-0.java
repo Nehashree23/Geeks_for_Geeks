3d531e3060ce1357893b96a829523f6cb800bba1
Neha Shree Kolluru <165762589+Nehashree23@users.noreply.github.com>
2026-07-15 23:22:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find if +ve, -ve or 0/find-if-ve-ve-or-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    void fun(int a) {
+        // code here
+        if(a==0){
+            System.out.print("Zero");
+        }
+        else if(a>=0){
+            System.out.print("Positive");
+        }
+        else{
+             System.out.print("Negative");
+        }
+    }
+}
